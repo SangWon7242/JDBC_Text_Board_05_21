f1b184d1fcb489ec2155913a90c108e627c00528
SangWon7242 <gotkdqja@gmail.com>
2023-05-21 14:51:57 +0900

    C:/Program Files/Git/usr/article/write 구현

--- src/main/java/com/sbs/exam/board/App.java
+++ src/main/java/com/sbs/exam/board/App.java
@@ -2,17 +2,36 @@ package com.sbs.exam.board;
 
 import com.sbs.exam.board.container.Container;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
   public void run() {
     Scanner sc = Container.scanner;
+    List<Article> articles = new ArrayList<>();
+    int articleLastId = 0;
 
     while (true) {
       System.out.printf("명령어) ");
       String cmd = sc.nextLine();
 
-      if(cmd.equals("article list")) {
+      if(cmd.equals("/usr/article/write")) {
+        System.out.println("== 게시물 등록 ==");
+        System.out.printf("제목) ");
+        String title = sc.nextLine();
+        System.out.printf("내용) ");
+        String content = sc.nextLine();
+        int id = ++articleLastId;
+
+        Article article = new Article(id, title, content);
+        System.out.println("생성된 게시물 객체 : " + article);
+
+        articles.add(article);
+
+        System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
+      }
+      else if(cmd.equals("/usr/article/list")) {
         System.out.println("== 게시물 리스트 ==");
       }
       else if(cmd.equals("system exit")) {

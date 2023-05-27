797f4516901b0cff838695e3cb5a8ab457e24874
SangWon7242 <gotkdqja@gmail.com>
2023-05-27 14:49:09 +0900

    게시물 리스트 데이터를 DB에서 가져오기

--- src/main/java/com/sbs/exam/board/App.java
+++ src/main/java/com/sbs/exam/board/App.java
@@ -2,10 +2,7 @@ package com.sbs.exam.board;
 
 import com.sbs.exam.board.container.Container;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -13,7 +10,6 @@ import java.util.Scanner;
 public class App {
   public void run() {
     Scanner sc = Container.scanner;
-    List<Article> articles = new ArrayList<>();
     int articleLastId = 0;
 
     while (true) {
@@ -70,14 +66,68 @@ public class App {
           }
         }
 
-        Article article = new Article(id, title, content);
-        System.out.println("생성된 게시물 객체 : " + article);
-
-        articles.add(article);
-
-        System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
+        System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
       }
       else if(cmd.equals("/usr/article/list")) {
+        Connection conn = null;
+        PreparedStatement pstat = null;
+        ResultSet rs = null;
+
+        List<Article> articles = new ArrayList<>();
+
+        try {
+          Class.forName("com.mysql.jdbc.Driver");
+
+          String url = "jdbc:mysql://127.0.0.1:3306/text_board?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+          conn = DriverManager.getConnection(url, "sbsst", "sbs123414");
+
+          String sql = "SELECT *";
+          sql += " FROM article";
+          sql += " ORDER BY id DESC;";
+
+          pstat = conn.prepareStatement(sql);
+          rs = pstat.executeQuery(sql);
+
+          while (rs.next()) {
+            int id = rs.getInt("id");
+            String regDate = rs.getString("regDate");
+            String updateDate = rs.getString("updateDate");
+            String title = rs.getString("title");
+            String content = rs.getString("content");
+
+            Article article = new Article(id, regDate, updateDate, title, content);
+            articles.add(article);
+          }
+
+        } catch (ClassNotFoundException e) {
+          System.out.println("드라이버 로딩 실패");
+        } catch (SQLException e) {
+          System.out.println("에러: " + e);
+        } finally {
+          try {
+            if (rs != null && !rs.isClosed()) {
+              rs.close();
+            }
+          } catch (SQLException e) {
+            e.printStackTrace();
+          }
+          try {
+            if (pstat != null && !pstat.isClosed()) {
+              pstat.close();
+            }
+          } catch (SQLException e) {
+            e.printStackTrace();
+          }
+          try {
+            if (conn != null && !conn.isClosed()) {
+              conn.close();
+            }
+          } catch (SQLException e) {
+            e.printStackTrace();
+          }
+        }
+
         System.out.println("== 게시물 리스트 ==");
 
         if(articles.isEmpty()) {

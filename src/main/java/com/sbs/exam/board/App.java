52e44c9b69f9fae543e80b61730b12297417c264
SangWon7242 <gotkdqja@gmail.com>
2023-05-27 12:41:54 +0900

    게시물 리스트 구현

--- src/main/java/com/sbs/exam/board/App.java
+++ src/main/java/com/sbs/exam/board/App.java
@@ -33,6 +33,18 @@ public class App {
       }
       else if(cmd.equals("/usr/article/list")) {
         System.out.println("== 게시물 리스트 ==");
+
+        if(articles.isEmpty()) {
+          System.out.println("게시물이 존재하지 않습니다.");
+          continue;
+        }
+
+        System.out.println("번호 / 제목");
+
+        for(Article article : articles) {
+          System.out.printf("%d / %s\n", article.id, article.title);
+        }
+
       }
       else if(cmd.equals("system exit")) {
         System.out.println("프로그램 종료");
